b3a33441bebc144074642cfc03310c3a6c0d770d
Beatbroetchen88 <sebastianrehberg.rehberg@tn.techstarter.de>
2025-05-16 11:56:07 +0200

    update database

---
+++ backend/DatabaseExport/bookbay_category0.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bookbay
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Abenteuer & Outdoor'),(2,'Autobiografie'),(3,'Backbuch'),(5,'Bilderbuch'),(4,'Biografie & Memoiren'),(6,'Business & Karriere'),(7,'Comics & Graphic Novels'),(8,'Dystopie'),(9,'Erstleser'),(10,'Fantasy'),(11,'Gesundheit & Ernährung'),(12,'Grusel & Spuk'),(13,'High Fantasy'),(14,'Historisch'),(15,'Historischer Roman'),(16,'Horror'),(17,'Jugendbuch'),(18,'Kinderbuch'),(19,'Kochbuch'),(20,'Krimi & Thriller'),(21,'Kunst & Kultur'),(22,'Liebesroman'),(23,'Manga'),(24,'Musik & Film'),(25,'New Adult'),(26,'Philosophie'),(27,'Politik & Gesellschaft'),(28,'Psychologie'),(29,'Psychothriller'),(30,'Ratgeber'),(31,'Reise'),(32,'Roman'),(33,'Romantasy'),(34,'Sachbuch'),(35,'Science-Fiction'),(36,'Spiritualität & Esoterik'),(37,'Steampunk'),(38,'Umwelt & Nachhaltigkeit'),(39,'Urban Fantasy'),(40,'Webtoon'),(41,'Wirtschaft & Finanzen'),(42,'Young Adult');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-16 11:55:43
